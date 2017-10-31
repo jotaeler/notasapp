31d8ccfb6707030c7e1eccc1dd28a3f781f37edc
cjsm0004 <carserrano46@gmail.com>
2017-10-31 17:59:39 +0100

    Migración de usuarios y creación de los mismos usando Faker

---
+++ notas/config/Migrations/20171031151440_CreateUsersTable.php
@@ -0,0 +1,40 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table -> addColumn ('name','string',[
+                             'limit' => 100,
+                             'null'=> false]);
+
+        $table -> addColumn ('username','string',[
+                             'limit' => 50,
+                             'null' => false]);
+
+        $table -> addColumn ('password','string',[
+                             'limit' => 50,
+                             'null' => false]);
+
+        $table -> addColumn ('created','datetime',[
+                             'null' => false]);
+
+        $table -> addColumn ('modified','datetime',[
+                             'null' => false]);
+
+        $table -> addColumn ('role','enum',[
+                             'values' => 'admin,user']);
+
+        $table -> create();
+
+    }
+}
